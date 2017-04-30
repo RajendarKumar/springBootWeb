b9f44ca369664ca57483a70129ec027ce556299d
RajendarKumar <mail2rajendar.kumar@gmail.com>
2017-04-30 23:45:30 +0530

    hello word with restfull service.

--- src/main/java/com/raj/BootWebApplication.java
+++ src/main/java/com/raj/BootWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BootWebApplication.class, args);
 	}
